ee26d0bde6856bc2d064c84f036d28ebd0554512
Evan Lucas <evanlucas@me.com>
2016-05-04 09:51:49 -0500

    builder: alphabetize methods
    Makes it easier to reason about the code

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const D = ast.declaration
 
 const id = ast.identifier
 
+Builder.array = E.ARRAY
 Builder.ast = ast
+Builder.id = id
+Builder.number = ast.number
+Builder.string = ast.string
+Builder.this = E.THIS
 
 module.exports = Builder
 
@@ -16,28 +21,6 @@ function Builder() {
     return new Builder()
 
   this.body = []
-}
-
-Builder.id = id
-
-Builder.prototype.program = function program() {
-  this.type = 'Program'
-  return this
-}
-
-Builder.block = function(body) {
-  if (body && !Array.isArray(body)) {
-    body = [body]
-  }
-  return S.BLOCK(body)
-}
-
-Builder.prototype.block = function block(body) {
-  return this.push(Builder.block(body))
-}
-
-Builder.prototype.reset = function reset() {
-  return new Builder()
 }
 
 Builder.prototype.push = function push() {
@@ -51,81 +34,18 @@ Builder.prototype.build = function build() {
   return this.body
 }
 
-Builder.prototype.use = function use(n) {
-  const str = `use ${n}`
-  const ele = S.EXPRESSION(ast.string(str))
-  return this.push(ele)
+Builder.prototype.reset = function reset() {
+  return new Builder()
 }
 
-Builder.require = function(str, prop) {
-  const callee = ast.identifier('require')
-  const c = E.CALL(callee, [
-    Builder.string(str)
-  ])
-
-  if (prop) {
-    return E.MEMBER(c, ast.identifier(prop), false)
-  }
-
-  return c
+Builder.prototype.program = function program() {
+  this.type = 'Program'
+  return this
 }
 
-Builder.prototype.require = function(str, prop) {
-  return this.push(Builder.require(str, prop))
-}
-
-Builder.string = ast.string
-Builder.number = ast.number
-
-Builder.prototype.string = function string(val) {
-  return this.push(Builder.string(val))
-}
-
-Builder.prototype.number = function number(val) {
-  return this.push(Builder.number(val))
-}
-
-Builder.regex = function(re) {
-  const str = re.toString()
-  let pattern = str.slice(1)
-  const flags = str.match(/[gimuy]*$/)[0]
-  if (flags.length) {
-    pattern = pattern.slice(0, -flags.length - 1)
-  } else {
-    pattern = pattern.slice(0, -1)
-  }
-  const r = new RegExp(pattern, flags)
-  return ast.regex(r, str, pattern, flags)
-}
-
-Builder.prototype.regex = function regex(re) {
-  return this.push(Builder.regex(re))
-}
-
-Builder.declare = function(type, name, val) {
-  return D.VARIABLE([
-    { type: 'VariableDeclarator'
-    , id: ast.identifier(name)
-    , init: val
-    }
-  ], type)
-}
-
-Builder.prototype.declare = function declare(type, name, val) {
-  return this.push(Builder.declare(type, name, val))
-}
-
-Builder.module = function(name) {
-  return S.EXPRESSION(E.ASSIGNMENT(
-    E.MEMBER(id('module'), id('exports'), false)
-  , '='
-  , id(name)
-  ))
-}
-
-Builder.prototype.module = function(name) {
-  return this.push(Builder.module(name))
-}
+// Please do not add any additional helpers above this line.
+// When adding a new helper, please put them in alphabetical order.
+// It makes digging through the code significantly easier.
 
 Builder.assign = function(key, val, op) {
   op = op || '='
@@ -140,10 +60,40 @@ Builder.prototype.assign = function assign(key, val, op) {
   return this.push(Builder.assign(key, val, op))
 }
 
-Builder.array = E.ARRAY
-
 Builder.prototype.array = function array(items) {
   return this.push(Builder.array(items))
+}
+
+Builder.block = function(body) {
+  if (body && !Array.isArray(body)) {
+    body = [body]
+  }
+  return S.BLOCK(body)
+}
+
+Builder.prototype.block = function block(body) {
+  return this.push(Builder.block(body))
+}
+
+Builder.callFunction = function(fnName, args) {
+  return E.CALL(ast.objectPath(fnName), args)
+}
+
+Builder.prototype.callFunction = function callFunction(fnName, args) {
+  return this.push(Builder.callFunction(fnName, args))
+}
+
+Builder.declare = function(type, name, val) {
+  return D.VARIABLE([
+    { type: 'VariableDeclarator'
+    , id: ast.identifier(name)
+    , init: val
+    }
+  ], type)
+}
+
+Builder.prototype.declare = function declare(type, name, val) {
+  return this.push(Builder.declare(type, name, val))
 }
 
 Builder.function = function(name, args, body) {
@@ -172,14 +122,6 @@ Builder.prototype.if = function(test, block, alt) {
   return this.push(Builder.if(test, block, alt))
 }
 
-Builder.not = function(arg) {
-  return E.UNARY('!', arg, true)
-}
-
-Builder.prototype.not = function not(arg) {
-  return this.push(Builder.not(arg))
-}
-
 Builder.ifNot = function(test, block, alt) {
   return Builder.if(Builder.not(test), block, alt)
 }
@@ -196,14 +138,16 @@ Builder.prototype.instanceOf = function instanceOf(left, right) {
   return this.push(Buffer.instanceOf(left, right))
 }
 
-Builder.this = E.THIS
-
-Builder.returns = function(arg) {
-  return S.RETURN(arg)
+Builder.module = function(name) {
+  return S.EXPRESSION(E.ASSIGNMENT(
+    E.MEMBER(id('module'), id('exports'), false)
+  , '='
+  , id(name)
+  ))
 }
 
-Builder.prototype.returns = function returns(arg) {
-  return this.push(Builder.returns(arg))
+Builder.prototype.module = function(name) {
+  return this.push(Builder.module(name))
 }
 
 Builder.new = function(fnName, args) {
@@ -220,12 +164,70 @@ Builder.prototype.new = function(fnName, args) {
   return this.push(Builder.new(fnName, args))
 }
 
-Builder.callFunction = function(fnName, args) {
-  return E.CALL(ast.objectPath(fnName), args)
+Builder.not = function(arg) {
+  return E.UNARY('!', arg, true)
 }
 
-Builder.prototype.callFunction = function callFunction(fnName, args) {
-  return this.push(Builder.callFunction(fnName, args))
+Builder.prototype.not = function not(arg) {
+  return this.push(Builder.not(arg))
+}
+
+Builder.prototype.number = function number(val) {
+  return this.push(Builder.number(val))
+}
+
+Builder.or = function(left, right) {
+  return E.LOGICAL(left, '||', right)
+}
+
+Builder.prototype.or = function or(left, right) {
+  return this.push(Builder.or(left, right))
+}
+
+Builder.regex = function(re) {
+  const str = re.toString()
+  let pattern = str.slice(1)
+  const flags = str.match(/[gimuy]*$/)[0]
+  if (flags.length) {
+    pattern = pattern.slice(0, -flags.length - 1)
+  } else {
+    pattern = pattern.slice(0, -1)
+  }
+  const r = new RegExp(pattern, flags)
+  return ast.regex(r, str, pattern, flags)
+}
+
+Builder.prototype.regex = function regex(re) {
+  return this.push(Builder.regex(re))
+}
+
+Builder.require = function(str, prop) {
+  const callee = ast.identifier('require')
+  const c = E.CALL(callee, [
+    Builder.string(str)
+  ])
+
+  if (prop) {
+    return E.MEMBER(c, ast.identifier(prop), false)
+  }
+
+  return c
+}
+
+Builder.prototype.require = function(str, prop) {
+  return this.push(Builder.require(str, prop))
+}
+
+Builder.returns = function(arg) {
+  return S.RETURN(arg)
+}
+
+Builder.prototype.returns = function returns(arg) {
+  return this.push(Builder.returns(arg))
+}
+
+Builder.prototype.string = function string(val) {
+  return this.push(Builder.string(val))
 }
 
 Builder.prototype.throws = function throws(arg) {
@@ -238,4 +240,10 @@ Builder.typeof = function(str) {
 
 Builder.prototype.typeof = function(str) {
   return this.push(Builder.typeof(str))
+}
+
+Builder.prototype.use = function use(n) {
+  const str = `use ${n}`
+  const ele = S.EXPRESSION(ast.string(str))
+  return this.push(ele)
 }
